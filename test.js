df1fc6826c75d0ff61689a8de15bb5798770a749
Clément <clement.nodet@gmail.com>
2011-01-02 17:27:08 +0100

    add `db` option

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ var assert = require('assert'),
     RedisStore = require('./index');
 
 var store = new RedisStore({ maxAge: 150000 });
+var store_alt = new RedisStore({ maxAge: 150000, db: 15 });
 
 store.client.on('connect', function(){
   // #set()
@@ -23,24 +24,31 @@ store.client.on('connect', function(){
           store.length(function(err, len){
               assert.ok(!err, '#length() got an error');
               assert.equal(1, len, '#length() with keys');
-      
-              // #clear()
-              store.clear(function(err, ok){
-                  assert.ok(!err, '#clear()');
-                  assert.ok(ok, '#clear()');
-      
-                  // #length()
-                  store.length(function(err, len){
-                      assert.ok(!err, '#length()');
-                      assert.equal(0, len, '#length() without keys');
-      
-                      // #set null
-                      store.set('123', { name: 'tj' }, function(){
-                          store.destroy('123', function(){
-                              store.length(function(err, len){
-                                 assert.equal(0, len, '#set() null');
-                                 console.log('done');
-                                 store.client.end(); 
+
+              // #db option
+              store_alt.length(function (err, len) {
+                  assert.ok(!err, '#alt db got an error');
+                  assert.equal(0, len, '#alt db with keys'); 
+
+                  // #clear()
+                  store.clear(function(err, ok){
+                      assert.ok(!err, '#clear()');
+                      assert.ok(ok, '#clear()');
+          
+                      // #length()
+                      store.length(function(err, len){
+                          assert.ok(!err, '#length()');
+                          assert.equal(0, len, '#length() without keys');
+          
+                          // #set null
+                          store.set('123', { name: 'tj' }, function(){
+                              store.destroy('123', function(){
+                                  store.length(function(err, len){
+                                     assert.equal(0, len, '#set() null');
+                                     console.log('done');
+                                     store.client.end(); 
+                                     store_alt.client.end();
+                                  });
                               });
                           });
                       });
