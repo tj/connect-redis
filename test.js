4a68f7fdb75fc3ae90f2c38a096983477c261dbb
Matthew King <matthew.y.king@gmail.com>
2011-09-05 13:55:57 -0400

    add options.client, key prefixes, events

--- test.js
+++ test.js
@@ -3,59 +3,53 @@
  * Module dependencies.
  */
 
-var assert = require('assert')
-  , connect = require('connect')
+var assert     = require('assert')
+  , redis      = require('redis')
+  , connect    = require('connect')
   , RedisStore = require('./')(connect);
 
-var store = new RedisStore;
-var store_alt = new RedisStore({ db: 15 });
+var client   = redis.createClient();
+var store    = new RedisStore(client);
+var storeAlt = new RedisStore(client, { prefix : 'alt:' }) 
+var sids     = [];
+
+store.on('set', function(sid, ttl){
+  sids.push(sid);
+})
+
+store.on('destroy', function(sid){
+  sids = sids.filter(function (x){ return x != sid; });
+})
 
 store.client.on('connect', function(){
   // #set()
   store.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(err, ok){
     assert.ok(!err, '#set() got an error');
     assert.ok(ok, '#set() is not ok');
+    assert.deepEqual(['123'], sids, '#set() did not emit event'); // assume expiry is not a factor 
     
     // #get()
     store.get('123', function(err, data){
       assert.ok(!err, '#get() got an error');
       assert.deepEqual({ cookie: { maxAge: 2000 }, name: 'tj' }, data);
   
-      // #length()
-      store.length(function(err, len){
-        assert.ok(!err, '#length() got an error');
-        assert.equal(1, len, '#length() with keys');
-
-        // #db option
-        store_alt.length(function (err, len) {
-          assert.ok(!err, '#alt db got an error');
-          assert.equal(0, len, '#alt db with keys'); 
-
-          // #clear()
-          store.clear(function(err, ok){
-            assert.ok(!err, '#clear()');
-            assert.ok(ok, '#clear()');
-
-            // #length()
-            store.length(function(err, len){
-              assert.ok(!err, '#length()');
-              assert.equal(0, len, '#length() without keys');
-
-              // #set null
-              store.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(){
-                store.destroy('123', function(){
-                  store.length(function(err, len){
-                   assert.equal(0, len, '#set() null');
-                   console.log('done');
-                   store.client.end(); 
-                   store_alt.client.end();
-                  });
-                });
-              });
+      // #destroy()
+      store.destroy('123', function(err){
+        assert.ok(!err, '#destroy() got an error');
+        assert.deepEqual([], sids, '#destroy() did not emit event');
+        store.get('123', function(err, data){
+          assert.ok(err, '#destroy() did not destroy the key');
+          
+          // options.prefix
+          storeAlt.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(err, ok){
+            client.get('alt:123', function(err, data){
+              assert.ok(!err, 'options.prefix is not prefixed to key');
+              console.log('done');
+              client.quit();
             });
           });
         });
       });
-    })
+    });
   });
 });
