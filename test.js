a5f2c25cf85ead7f678033080aa3b3c8e313c5ea
heyiyu.pt_52624 <heyiyu.pt@edp2.sqa.cm4>
2011-12-27 22:36:07 +0800

    update test, remove clear & length

--- test.js
+++ test.js
@@ -8,27 +8,34 @@ var assert = require('assert')
   , RedisStore = require('./')(connect);
 
 var store = new RedisStore;
-var store_alt = new RedisStore({ db: 15 });
 
 store.client.on('connect', function(){
   // #set()
   store.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(err, ok){
     assert.ok(!err, '#set() got an error');
     assert.ok(ok, '#set() is not ok');
-    
     // #get()
     store.get('123', function(err, data){
       assert.ok(!err, '#get() got an error');
+      //throw new Error('uncaughtException'); 
       assert.deepEqual({ cookie: { maxAge: 2000 }, name: 'tj' }, data);
-      throw new Error('uncaughtException');
+      store.destroy('123', function(){
+        store.get('123',function(err, data){
+          assert.ok(!err, '#get() null got an error');
+          assert.ok(!data, '#get() null got data error');
+          throw new Error('uncaughtException');  
+          store.client.end();
+        })
+      });
     })
   });
 });
 
 process.on('uncaughtException', function (err) {
-  if('Error: uncaughtException' === err.toString()){
+  if(err.message==='uncaughtException'){
     console.log('done');
   }else{
-    console.log('try catch error');
+    console.log(err.message);
   }
-});
+  store.client.end();
+}); 
