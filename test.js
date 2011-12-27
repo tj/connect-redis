b6503643ed52f68b03cfdbdc9f2917b526e1cf1b
heyiyu.pt_52624 <heyiyu.pt@edp2.sqa.cm4>
2011-12-27 21:59:19 +0800

    fix bug of get & update the test

--- test.js
+++ test.js
@@ -20,42 +20,15 @@ store.client.on('connect', function(){
     store.get('123', function(err, data){
       assert.ok(!err, '#get() got an error');
       assert.deepEqual({ cookie: { maxAge: 2000 }, name: 'tj' }, data);
-  
-      // #length()
-      store.length(function(err, len){
-        assert.ok(!err, '#length() got an error');
-        assert.equal(1, len, '#length() with keys');
-
-        // #db option
-        store_alt.length(function (err, len) {
-          assert.ok(!err, '#alt db got an error');
-          assert.equal(0, len, '#alt db with keys'); 
-
-          // #clear()
-          store.clear(function(err, ok){
-            assert.ok(!err, '#clear()');
-            assert.ok(ok, '#clear()');
-
-            // #length()
-            store.length(function(err, len){
-              assert.ok(!err, '#length()');
-              assert.equal(0, len, '#length() without keys');
-
-              // #set null
-              store.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(){
-                store.destroy('123', function(){
-                  store.length(function(err, len){
-                   assert.equal(0, len, '#set() null');
-                   console.log('done');
-                   store.client.end(); 
-                   store_alt.client.end();
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
+      throw new Error('uncaughtException');
     })
   });
+});
+
+process.on('uncaughtException', function (err) {
+  if('Error: uncaughtException' === err.toString()){
+    console.log('done');
+  }else{
+    console.log('try catch error');
+  }
 });
