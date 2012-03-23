44810dae0202197c4dea911aefb9b30bef06201d
TJ Holowaychuk <tj@vision-media.ca>
2012-03-23 14:23:58 -0700

    fixed tests

--- test.js
+++ test.js
@@ -21,39 +21,12 @@ store.client.on('connect', function(){
       assert.ok(!err, '#get() got an error');
       assert.deepEqual({ cookie: { maxAge: 2000 }, name: 'tj' }, data);
   
-      // #length()
-      store.length(function(err, len){
-        assert.ok(!err, '#length() got an error');
-        assert.equal(1, len, '#length() with keys');
-
-        // #db option
-        store_alt.length(function (err, len) {
-          assert.ok(!err, '#alt db got an error');
-          assert.equal(0, len, '#alt db with keys'); 
-
-          // #clear()
-          store.clear(function(err, ok){
-            assert.ok(!err, '#clear()');
-            assert.ok(ok, '#clear()');
-
-            // #length()
-            store.length(function(err, len){
-              assert.ok(!err, '#length()');
-              assert.equal(0, len, '#length() without keys');
-
-              // #set null
-              store.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(){
-                store.destroy('123', function(){
-                  store.length(function(err, len){
-                   assert.equal(0, len, '#set() null');
-                   console.log('done');
-                   store.client.end(); 
-                   store_alt.client.end();
-                  });
-                });
-              });
-            });
-          });
+      // #set null
+      store.set('123', { cookie: { maxAge: 2000 }, name: 'tj' }, function(){
+        store.destroy('123', function(){
+         console.log('done');
+         store.client.end(); 
+         store_alt.client.end();
         });
       });
     })
