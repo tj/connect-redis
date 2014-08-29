0fefffbde2d42a8559ffa46358daa3bc153dcd99
Marc Harter <wavded@gmail.com>
2014-08-29 11:13:05 -0500

    mod; bench (ocd)

--- bench/redisbench.js
+++ bench/redisbench.js
@@ -1,12 +1,12 @@
-var Promise = require('bluebird')
-var session = require('express-session')
-var RedisStore = require('connect-redis')(session)
+var Promise = require('bluebird');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
 
-var count = 100000
-var i=0
-var tasks = []
+var count = 100000;
+var i = 0;
+var tasks = [];
 
-console.time('bench'+count)
+console.time('bench '+count)
 
 var store = Promise.promisifyAll(new RedisStore({
   host: 'localhost',
@@ -14,10 +14,10 @@ var store = Promise.promisifyAll(new RedisStore({
 }));
 
 for (; i < count; i++) {
-  tasks.push(store.setAsync('testsession'+i, {cookie: {maxAge:2000}, name: 'sample name'}))
+  tasks.push(store.setAsync('testsession'+i, {cookie: {maxAge:2000}, name: 'sample name'}));
 }
 
 Promise.all(tasks).then(function() {
-  console.timeEnd('bench'+count);
+  console.timeEnd('bench '+count);
   process.exit(0);
 })
