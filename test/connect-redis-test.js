e6e7e18f6e921a3fd5c08d21743fa113cd4e64b5
Marc Harter <wavded@gmail.com>
2017-05-02 08:59:51 -0500

    Fix tests.

--- test/connect-redis-test.js
+++ test/connect-redis-test.js
@@ -10,10 +10,7 @@ var ioRedis = require('ioredis');
 function lifecycleTest (store, t) {
   P.promisifyAll(store);
 
-  return redisSrv.connect()
-    .then(function () {
-      return store.setAsync('123', { cookie: { maxAge: 2000 }, name: 'tj' });
-    })
+  return store.setAsync('123', { cookie: { maxAge: 2000 }, name: 'tj' })
     .then(function (ok) {
       t.equal(ok, 'OK', '#set() ok');
       return store.getAsync('123');
@@ -30,10 +27,11 @@ function lifecycleTest (store, t) {
     })
     .then(function (ok) {
       t.equal(ok, 1, '#destroy() ok');
-      store.client.end();
-      return redisSrv.disconnect();
+      store.client.end(false);
     });
 }
+
+test('setup', redisSrv.connect);
 
 test('defaults', function (t) {
   var store = new RedisStore();
@@ -42,24 +40,24 @@ test('defaults', function (t) {
   t.notOk(store.disableTTL, 'disableTTL not set');
   t.ok(store.client, 'creates client');
 
-  store.client.end();
+  store.client.end(false);
   t.end();
 });
 
 test('basic', function (t) {
   t.throws(RedisStore, TypeError, 'constructor not callable as function');
-  var store = new RedisStore({ port: 8543 });
+  var store = new RedisStore({ port: redisSrv.port });
   return lifecycleTest(store, t);
 });
 
 test('existing client', function (t) {
-  var client = redis.createClient(8543, 'localhost');
+  var client = redis.createClient(redisSrv.port, 'localhost');
   var store = new RedisStore({ client: client });
   return lifecycleTest(store, t);
 });
 
 test('io redis client', function (t) {
-  var client = ioRedis.createClient(8543, 'localhost');
+  var client = ioRedis.createClient(redisSrv.port, 'localhost');
   var store = new RedisStore({ client: client });
   return lifecycleTest(store, t);
 });
@@ -67,7 +65,7 @@ test('io redis client', function (t) {
 test('options', function (t) {
   var store = new RedisStore({
     host: 'localhost',
-    port: 8543,
+    port: redisSrv.port,
     prefix: 'tobi',
     ttl: 1000,
     disableTTL: true,
@@ -80,29 +78,29 @@ test('options', function (t) {
   t.equal(store.ttl, 1000, 'ttl set');
   t.ok(store.disableTTL, 'disableTTL set');
   t.ok(store.client, 'creates client');
-  t.equal(store.client.address, 'localhost:8543', 'sets host and port');
+  t.equal(store.client.address, 'localhost:'+redisSrv.port, 'sets host and port');
 
   var socketStore = new RedisStore({ socket: 'word' });
   t.equal(socketStore.client.address, 'word', 'sets socket address');
-  socketStore.client.end();
+  socketStore.client.end(false);
 
   var urlStore = new RedisStore({ url: 'redis://127.0.0.1:8888' });
   t.equal(urlStore.client.address, '127.0.0.1:8888', 'sets url address');
-  urlStore.client.end();
+  urlStore.client.end(false);
 
   var hostNoPort = new RedisStore({ host: 'host' });
   t.equal(hostNoPort.client.address, 'host:6379', 'sets default port');
-  hostNoPort.client.end();
+  hostNoPort.client.end(false);
 
   return lifecycleTest(store, t);
 });
 
 test('interups', function (t) {
-  var store = P.promisifyAll(new RedisStore({ port: 8543, connect_timeout: 500 }));
+  var store = P.promisifyAll(new RedisStore({ port: redisSrv.port, connect_timeout: 500 }));
   return store.setAsync('123', { cookie: { maxAge: 2000 }, name: 'tj' })
     .catch(function (er) {
       t.ok(/broken/.test(er.message), 'failed connection');
-      store.client.end();
+      store.client.end(false);
     });
 });
 
@@ -117,6 +115,8 @@ test('serializer', function (t) {
   t.equal(serializer.stringify('UnitTest'), 'XXX"UnitTest"');
   t.equal(serializer.parse(serializer.stringify('UnitTest')), 'UnitTest');
 
-  var store = new RedisStore({ port: 8543, serializer: serializer });
+  var store = new RedisStore({ port: redisSrv.port, serializer: serializer });
   return lifecycleTest(store, t);
 });
+
+test('teardown', redisSrv.disconnect);
--- test/redis-server.js
+++ test/redis-server.js
@@ -1,21 +1,17 @@
 var P = require('bluebird');
 var spawn = require('child_process').spawn;
 var redisSrv;
-var port = exports.port = 8543;
+var port = exports.port = 18543;
 
 exports.connect = function () {
-  if (redisSrv) return P.resolve();
-
-  redisSrv = spawn('redis-server', ['--port', port, '--loglevel', 'verbose'], { stdio: 'ignore' });
-  return P.delay(1000);
+  redisSrv = spawn('redis-server', [
+    '--port', port,
+    '--loglevel', 'notice',
+  ], { stdio: 'inherit' });
+  return P.delay(1500);
 };
 
 exports.disconnect = function () {
-  if (!redisSrv) return P.resolve();
-
-  return new P(function (res) {
-    redisSrv.kill();
-    redisSrv.once('close', res);
-    redisSrv = null;
-  });
+  redisSrv.kill('SIGKILL');
+  return P.resolve();
 };
