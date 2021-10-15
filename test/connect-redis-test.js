0fbca577598c46ee6896e8a08cf1861ae1ab48ca
Aaron Hutchinson <xarnze@gmail.com>
2021-10-15 15:20:29 +0100

    Support for concurrent sessions (#333)
    This fixes an issue where multiple requests modifying the session are in progress at the same time, before this fix the last one to complete looses the changes made by the other in flight request. After this fix changes are merged in from all in flight requests.
    
    https://github.com/tj/connect-redis/issues/328

--- test/connect-redis-test.js
+++ test/connect-redis-test.js
@@ -4,8 +4,10 @@ const session = require('express-session')
 const redis = require('redis')
 const ioRedis = require('ioredis')
 const redisMock = require('redis-mock')
+const MockDate = require('mockdate')
 
 let RedisStore = require('../')(session)
+MockDate.set('2000-11-22')
 
 let p =
   (ctx, method) =>
@@ -58,11 +60,34 @@ test('redis-mock client', async (t) => {
 test('teardown', redisSrv.disconnect)
 
 async function lifecycleTest(store, t) {
-  let res = await p(store, 'set')('123', { foo: 'bar' })
+  await p(store, 'set')('123', { foo: 'bar3' })
+  let res = await p(store, 'get')('123')
+  t.same(res, { foo: 'bar3', lastModified: 974851200000 }, 'get value 1')
+  await p(store, 'set')('123', {
+    foo: 'bar3',
+    luke: 'skywalker',
+    obi: 'wan',
+    lastModified: 974851000000,
+  })
+  await p(store, 'set')('123', {
+    luke: 'skywalker',
+    lastModified: 974851000000,
+  })
+  res = await p(store, 'get')('123')
+  t.same(
+    res,
+    { foo: 'bar3', luke: 'skywalker', obi: 'wan', lastModified: 974851200000 },
+    'get merged value'
+  )
+
+  res = await p(store, 'clear')()
+  t.ok(res >= 1, 'cleared key')
+
+  res = await p(store, 'set')('123', { foo: 'bar' })
   t.equal(res, 'OK', 'set value')
 
   res = await p(store, 'get')('123')
-  t.same(res, { foo: 'bar' }, 'get value')
+  t.same(res, { foo: 'bar', lastModified: 974851200000 }, 'get value')
 
   res = await p(store.client, 'ttl')('sess:123')
   t.ok(res >= 86399, 'check one day ttl')
@@ -96,8 +121,8 @@ async function lifecycleTest(store, t) {
   t.same(
     res,
     [
-      { id: '123', foo: 'bar' },
-      { id: '456', cookie: { expires } },
+      { id: '123', foo: 'bar', lastModified: 974851200000 },
+      { id: '456', cookie: { expires }, lastModified: 974851200000 },
     ],
     'stored two keys data'
   )
@@ -105,11 +130,20 @@ async function lifecycleTest(store, t) {
   res = await p(store, 'destroy')('456')
   t.equal(res, 1, 'destroyed one')
 
+  res = await p(store, 'get')('456')
+  t.equal(res, undefined, 'tombstoned one')
+
+  res = await p(store, 'set')('456', { a: 'new hope' })
+  t.equal(res, undefined, 'tombstoned set')
+
+  res = await p(store, 'get')('456')
+  t.equal(res, undefined, 'tombstoned two')
+
   res = await p(store, 'length')()
   t.equal(res, 1, 'one key remains')
 
   res = await p(store, 'clear')()
-  t.equal(res, 1, 'cleared remaining key')
+  t.equal(res, 2, 'cleared remaining key')
 
   res = await p(store, 'length')()
   t.equal(res, 0, 'no key remains')
