cf1ae764e9dbf74d8758a531d60add5f14c51570
Sahil Borhade <sahil.borhade@nonstopio.com>
2025-03-17 17:17:36 +0530

    test-gen-config.json

--- index_test.ts
+++ index_test.ts
@@ -10,4 +10,19 @@ test("setup", async () => {
   await redisSrv.connect()
 })
 
+test("defaults", async () => {
+  let client = createClient({url: `redis://localhost:${redisSrv.port}`})
+  await client.connect()
+
+  let store = new RedisStore({client})
+
+  expect(store.client).toBeDefined()
+  expect(store.prefix).toBe("sess:")
+  expect(store.ttl).toBe(86400) // defaults to one day
+  expect(store.scanCount).toBe(100)
+  expect(store.serializer).toBe(JSON)
+  expect(store.disableTouch).toBe(false)
+  expect(store.disableTTL).toBe(false)
+  await client.disconnect()
+})
 
