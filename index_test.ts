3a8cb89fcf7e2916a6c757e7b129d3f9c744aefb
Farhankhan Daya <lobabob@users.noreply.github.com>
2025-05-07 14:36:50 -0500

    Fixed newly surfaced typing issues in tests.

--- index_test.ts
+++ index_test.ts
@@ -45,7 +45,7 @@ test("teardown", redisSrv.disconnect)
 async function lifecycleTest(store: RedisStore, client: any): Promise<void> {
   let res = await store.clear()
 
-  let sess = {foo: "bar"}
+  let sess = {foo: "bar", cookie: {originalMaxAge: null}}
   await store.set("123", sess)
 
   res = await store.get("123")
@@ -55,14 +55,14 @@ async function lifecycleTest(store: RedisStore, client: any): Promise<void> {
   expect(ttl).toBeGreaterThanOrEqual(86399)
 
   ttl = 60
-  let expires = new Date(Date.now() + ttl * 1000).toISOString()
-  await store.set("456", {cookie: {expires}})
+  let expires = new Date(Date.now() + ttl * 1000)
+  await store.set("456", {cookie: {originalMaxAge: null, expires}})
   ttl = await client.ttl("sess:456")
   expect(ttl).toBeLessThanOrEqual(60)
 
   ttl = 90
-  let expires2 = new Date(Date.now() + ttl * 1000).toISOString()
-  await store.touch("456", {cookie: {expires: expires2}})
+  let expires2 = new Date(Date.now() + ttl * 1000)
+  await store.touch("456", {cookie: {originalMaxAge: null, expires: expires2}})
   ttl = await client.ttl("sess:456")
   expect(ttl).toBeGreaterThan(60)
 
@@ -76,8 +76,8 @@ async function lifecycleTest(store: RedisStore, client: any): Promise<void> {
   res = await store.all()
   res.sort((a: any, b: any) => (a.id > b.id ? 1 : -1))
   expect(res).toEqual([
-    {id: "123", foo: "bar"},
-    {id: "456", cookie: {expires}},
+    {id: "123", foo: "bar", cookie: {originalMaxAge: null}},
+    {id: "456", cookie: {originalMaxAge: null, expires: expires.toISOString()}},
   ])
 
   await store.destroy("456")
@@ -99,14 +99,14 @@ async function lifecycleTest(store: RedisStore, client: any): Promise<void> {
   res = await store.length()
   expect(res).toBe(0)
 
-  expires = new Date(Date.now() + ttl * 1000).toISOString() // expires in the future
-  res = await store.set("789", {cookie: {expires}})
+  expires = new Date(Date.now() + ttl * 1000) // expires in the future
+  res = await store.set("789", {cookie: {originalMaxAge: null, expires}})
 
   res = await store.length()
   expect(res).toBe(1)
 
-  expires = new Date(Date.now() - ttl * 1000).toISOString() // expires in the past
-  await store.set("789", {cookie: {expires}})
+  expires = new Date(Date.now() - ttl * 1000) // expires in the past
+  await store.set("789", {cookie: {originalMaxAge: null, expires}})
 
   res = await store.length()
   expect(res).toBe(0) // no key remains and that includes session 789
